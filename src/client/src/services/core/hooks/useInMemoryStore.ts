41cc94fa678d1bb392e4083145c840048b3f245e
Stanislav <Silin>
2020-09-08 21:48:55 +0300

    Update store.

--- src/client/src/services/core/hooks/useInMemoryStore.ts
+++ src/client/src/services/core/hooks/useInMemoryStore.ts
@@ -1,15 +1,32 @@
-import { createGlobalState } from 'react-hooks-global-state'
+import { useState, useEffect } from 'react'
 
-export function createInMemoryStore<TValue>(value: TValue) {
-  let key = Object.keys(value)[0]
-  let stateKey = key as keyof TValue
-  let { useGlobalState } = createGlobalState(value)
-  function useStore() {
-    let [state, setState] = useGlobalState(stateKey)
-    function newSetState(state: TValue[keyof TValue]) {
-      setState(state)
-    }
-    return [state, newSetState] as [typeof state, typeof newSetState]
+export function createInMemoryStore<TValue>(initialValue: TValue) {
+  class State {
+    callbacks = new Set<(newValue: TValue) => void>()
+    constructor(public value: TValue) { }
   }
+
+  let state = new State(initialValue)
+
+  function useStore() {
+    let [hookValue, setHookValue] = useState(state.value)
+    state.callbacks.add(setHookValue)
+
+    function setState(newValue: TValue) {
+      state.value = newValue
+      for (const callbacks of state.callbacks) {
+        callbacks(newValue)
+      }
+    }
+
+    useEffect(() => {
+      return () => {
+        state.callbacks.delete(setHookValue)
+      }
+    }, [hookValue])
+
+    return [hookValue, setState] as [TValue, typeof setState]
+  }
+
   return useStore
 }
